f4b7db9fe2779a5646b75c41e5bff3897a1be08e
Jowita <jowita.mielnicka@gmail.com>
2020-02-16 21:19:45 +0100

    zmiany wizualizacji bladzenia losowego

--- random_walk_visual.py
+++ random_walk_visual.py
@@ -5,12 +5,15 @@ from random_walk import RandomWalk
 # tworzenie nowego bladzenia losowego dopoki program aktywny
 while True:
     # przygotowanie danych bladzenia losowego i wyswietlenie punktow
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
+
+    # okreslenie wielkosci okna wykresu
+    plt.figure(figsize=(10, 6), facecolor='green', dpi=128)
 
     point_numbers = list(range(rw.num_points))
 
-    plt.scatter(rw.x_values, rw.y_values, s=15, c=point_numbers, cmap=plt.cm.Greens, edgecolors='none')
+    plt.scatter(rw.x_values, rw.y_values, s=1, c=point_numbers, cmap=plt.cm.Greens, edgecolors='none')
 
     # podkreslenie pierwszego i ostatniego punktu
     plt.scatter(0, 0, c='red', edgecolors='none', s=50)
@@ -21,7 +24,9 @@ while True:
     plt.yticks([])
 
     plt.show()
-
-    keep_running = input("Utworzyć kolejne błądzenie losowe? (t/n): ")
-    if keep_running == "n":
+    try:
+        keep_running = input("Utworzyć kolejne błądzenie losowe? (t/n): ")
+        if keep_running == "n":
+            break
+    except KeyboardInterrupt:
         break
