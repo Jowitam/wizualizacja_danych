b312ad6761e7f4f3cd4e4a51669aaee6259b134c
Jowita <jowita.mielnicka@gmail.com>
2020-03-20 17:23:30 +0100

    wizualizacja populacji calego swiata 2010

--- format_json/world_population.py
+++ format_json/world_population.py
@@ -1,5 +1,6 @@
 import json
 
+from pygal.maps.world import World
 from format_json.country_codes import get_country_code
 
 # wczytywanie danych z format_json i wrzut ich na liste
@@ -7,13 +8,20 @@ filname = 'population_data.json'
 with open(filname) as f:
     pop_data = json.load(f)
 
-# wyswietlanie polulacji dla panstw w 2010r
+# utworzenie slownika danych dotyczacych populacji
+cc_population = {}
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))
         code = get_country_code(country_name)
         if code:
-            print(code + ": " + str(population))
+            cc_population[code] = population
         else:
             print("BŁĄD - " + country_name)
+
+wm = World()
+wm.force_uri_protocol = 'http'
+wm.title = 'Populacja na świecie 2010'
+wm.add('2010', cc_population)
+wm.render_to_file('world_population.svg')
